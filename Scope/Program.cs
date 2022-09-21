1bd19250b17f5751b7461c2d3c4b86f87692b7aa
fulfordcs <fulfordcs@gmail.com>
2022-09-21 13:57:22 +0100

    End of lesson

--- Scope/Program.cs
+++ Scope/Program.cs
@@ -8,20 +8,39 @@ namespace Scope
 {
     class Program
     {
-        static string language = "English";
+        static string title = "Translator";
+        static string name;
 
         static void Main(string[] args)
         {
-            SayHello("English");
-            SayHello("German");
-            SayHello("French");
-            SayHello("Yorkshire");
-            
+            ShowOff();
+            ShowOff();
+            ShowOff();
+            ShowOff();
+            ShowOff();
+            ShowOff();
+            ShowOff();
+            ShowOff();
+
+        }
+
+        private static void ShowOff()
+        {
+            List<string> languages = new List<string> {
+                "English", "German", "French", "Yorkshire"
+            };
+            Console.WriteLine(title);
+
+            foreach (string language in languages)
+            {
+                SayHello(language);
+                // make it say "You can greet people in English by saying 'Hello'"
+            }
         }
 
         private static void SayHello(string language)
         {
-            Console.WriteLine(language);
+            Console.Write($"You can say a greeting in {language} by saying ");
             switch (language)
             {
                 case "English":
