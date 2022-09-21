7d0c1d9640c336eafea1f23f520b9b6d708bc31a
fulfordcs <fulfordcs@gmail.com>
2022-09-21 12:02:44 +0100

    Add project files.

---
+++ Scope/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHello("English");
+            SayHello("German");
+            SayHello("French");
+            SayHello("Yorkshire");
+        }
+
+        private static void SayHello(string language)
+        {
+            switch(language)
+            {
+                case "English":
+                    Console.WriteLine("Hello");
+                    break;
+                case "German":
+                    Console.WriteLine("Guten tag");
+                    break;
+                case "French":
+                    Console.WriteLine("Bonjour");
+                    break;
+                default:
+                    Console.WriteLine("How do?!");
+                    break;
+            }
+        }
+    }
+}
